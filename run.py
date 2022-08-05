65cb507a3636f16f093da74f7e7e477ca89fea08
SWCA-Jesse-Ellin <jesse.ellin@swca.com>
2022-08-05 08:47:31 -0400

    Run v0.0

--- run.py
+++ run.py
@@ -6,7 +6,36 @@ import pandas as pd
 import pathlib
 
 from models.bilstm import BiLSTMModel
+from models.constants import TOKENIZER_TIMESTAMP, MODEL_TIMESTAMP, SEQUENCE_LEN
+from pipeline.pipe import Pipeline
 
 parser = argpare.ArgumentParser()
 parser.add_argument("-f", "--input-file", type=pathlib.Path, required=True, help="Path to csv file that stores the input data")
 parser.add_argument("-k", "--primary-key", type=str, required=False, default="comment_text", help="String key used in the csv to denote letter text column")
+args = vars(parser.parse_args())
+
+pipeline = Pipeline()
+data = pipeline.process(args["--input-file"])
+
+with open(f"models/saved_models/tokenizer_{TOKENIZER_TIMESTAMP}.json") as f:
+	tokenizer = tokenizer_from_json(f.read())
+
+x = data[args["--primary-key"]]
+word_index = tokenier.word_index
+word_index = {k : (v+3) for k,v in word_index.items()}
+word_index["<PAD>"] = 0
+word_index["<START>"] = 1
+word_index["<UNK>"] = 2
+word_index["<UNUSED>"] = 3
+x_tokens = tokenier.texts_to_sequences(x)
+x_pad = pad_sequence(x_tokens, value=word_index["<PAD>"], padding="post", maxlen=SEQUENCE_LEN)
+
+model = BiLSTMModel(load_file=f"models/saved_models/LSTM_{MODEL_TIMESTAMP}")
+
+resutls = model.predict(x_pad)
+binary = model.to_binary(results, threshold=0.5)
+
+data["significance"] = binary
+data["confidence"] = results
+
+data.to_csv("binary_model_results.csv")
