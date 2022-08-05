7f9568a00dc163d527bd40df401f0e031bf7a109
SWCA-Jesse-Ellin <jesse.ellin@swca.com>
2022-08-05 08:41:00 -0400

    Argument parsing

--- run.py
+++ run.py
@@ -3,6 +3,10 @@ from tensorflow.keras.preprocessing.text import tokenizer_from_json
 from tensorflow.keras.preprocessing.sequence import pad_sequence
 from tensorflow.keras.models import load_model
 import pandas as pd
-import spacy
+import pathlib
 
 from models.bilstm import BiLSTMModel
+
+parser = argpare.ArgumentParser()
+parser.add_argument("-f", "--input-file", type=pathlib.Path, required=True, help="Path to csv file that stores the input data")
+parser.add_argument("-k", "--primary-key", type=str, required=False, default="comment_text", help="String key used in the csv to denote letter text column")
